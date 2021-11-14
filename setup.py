c06e4462719113f9268303e0d3ddcc5da22d5765
joowon-kim-cox <88127591+joowon-kim-cox@users.noreply.github.com>
2021-11-15 00:17:51 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='stylegan3',
-    packages=find_packages(),
+    version='0.1.0',
+    url='https://github.com/joowon-kim-cox/stylegan3.git',
+    packages=['stylegan3'],
+    zip_safe=False,
     description='stylegan3 for pip installation',
 )
